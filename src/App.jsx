f9ecb4829377b02d0addf3f04fdd72eb15547b1e
mnosumon <mdnuruddin378350@gmail.com>
2024-10-23 23:47:02 +0600

    Modal part design

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,40 @@
-import React from 'react'
-import {
-  createRoutesFromElements,
-  createBrowserRouter,
-  Route,
-  RouterProvider,
-} from "react-router-dom";
-
-import Registration from './pages/registration/idex';
-import Login from './pages/login/Login';
+import React, { useRef } from "react";
+import { IoMdCloseCircleOutline } from "react-icons/io";
+import { CiImageOn } from "react-icons/ci";
 
 const App = () => {
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <Route>
-        <Route path='/registration' element={<Registration/>} />
-        <Route path='/login' element={<Login/>} />
-      </Route>
-    )
-  );
-  return (
-    <RouterProvider router={router} />
-  )
-}
+  const fileRef = useRef();
 
-export default App
+  return (
+    <div className="w-full h-screen bg-[#7875755d] flex items-center justify-center ">
+      <div className="w-2/5 bg-white p-3 relative rounded-md">
+        <div className="w-full mb-4">
+          <h3 className="text-lg text-center font-serif text-[#2d2d2d]">
+            Upload Photo
+          </h3>
+          <div className="absolute right-2 top-2 cursor-pointer text-xl">
+            <IoMdCloseCircleOutline />
+          </div>
+        </div>
+        <div className="border border-[#a19e9e] rounded-md">
+          <div
+            onClick={() => fileRef.current.click()}
+            className="m-1 bg-slate-300 rounded-md h-60 flex items-center justify-center cursor-pointer"
+          >
+            <div className="flex justify-center items-center flex-col gap-1">
+              <div className="">
+                <CiImageOn />
+              </div>
+              <h4>Chose your photo</h4>
+            </div>
+            <div className="">
+              <input ref={fileRef} className="hidden" type="file" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
